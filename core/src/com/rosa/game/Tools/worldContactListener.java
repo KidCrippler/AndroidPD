3119364aa7253187a2b04e5052b03c6ef1fb0d8a
Adam Delarosa <gojiradam@gmail.com>
2016-08-24 01:17:14 +0300

    test

--- core/src/com/rosa/game/Tools/worldContactListener.java
+++ core/src/com/rosa/game/Tools/worldContactListener.java
@@ -7,8 +7,11 @@ import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.rosa.game.Sprites.InteractiveTileObject;
 
+
 public class WorldContactListener implements ContactListener{
 
+
+    
     @Override
     public void beginContact(Contact contact) {
         Fixture fixA = contact.getFixtureA();
@@ -20,7 +23,7 @@ public class WorldContactListener implements ContactListener{
 
             if(object.getUserData() instanceof InteractiveTileObject) {
                 ((InteractiveTileObject) object.getUserData()).onHeadHit();
-            }/
+            }
         }
     }
 
