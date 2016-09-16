867abc34db8753ff976d20ef8a4c2ff23c296e8e
ROSA <gojiradam@gmail.com>
2016-09-16 15:12:47 +0300

    Game Sounds

--- core/src/com/rosa/game/Tools/SoundPlayer.java
+++ core/src/com/rosa/game/Tools/SoundPlayer.java
@@ -4,22 +4,27 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.files.FileHandle;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SoundPlayer {
 
+    private Sound soundFXStream;
 
-  /*  public void soundPlayer(int soundNumber) {
+
+    public void soundPlayer(int soundNumber) {
 
 
-        List<String> soundString = new ArrayList<String>(10);
+        ArrayList<String> soundString = new ArrayList();
+
 
         soundString.add(1,"sounds/audio/Laser_Shoot.wav");
 
-        Sound soundFXStream = Gdx.audio.newSound(Gdx.files.internal(soundString.get(1)));
+        soundFXStream = Gdx.audio.newSound(Gdx.files.internal(soundString.get(1)));
 
-        System.out.println(soundFXStream);
+
+
         soundFXStream.play();
-    }*/
+    }
 }
