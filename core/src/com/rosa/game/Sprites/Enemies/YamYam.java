a12b66056047a3ca52ef7c2e41a432e83fde8765
adamdelarosa <gojiradam@gmail.com>
2016-11-27 01:01:57 +0200

    Menu build

--- core/src/com/rosa/game/Sprites/Enemies/YamYam.java
+++ core/src/com/rosa/game/Sprites/Enemies/YamYam.java
@@ -117,6 +117,7 @@ public class YamYam extends Enemy {
                 setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
                 setRegion(walkAnimation.getKeyFrame(stateTime, true));
 
+                //Fire bullets:
                 fire();
                 for (EnemyFirePowerLas enemyFirePowerLas : enemyFirePowerLasArray) {
                     enemyFirePowerLas.update(dt);
