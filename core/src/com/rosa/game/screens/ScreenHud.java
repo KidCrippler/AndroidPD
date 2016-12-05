68877138b4e5e2176364b911c3ee35113be25d56
adamdelarosa <gojiradam@gmail.com>
2016-12-05 22:31:22 +0200

    MenuBuild

--- core/src/com/rosa/game/screens/ScreenHud.java
+++ core/src/com/rosa/game/screens/ScreenHud.java
@@ -14,22 +14,21 @@ public class ScreenHud extends Sprite {
     private NinePatchDrawable loadingBarBackground;
     private NinePatchDrawable loadingBar;
 
-
     public ScreenHud() {
         TextureAtlas skinAtlas = new TextureAtlas(Gdx.files.internal("gdx_demo/data/uiskin.atlas"));
         NinePatch loadingBarBackgroundPatch = new NinePatch(skinAtlas.findRegion("default-round"), 1, 1, 1, 1);
         NinePatch loadingBarPatch = new NinePatch(skinAtlas.findRegion("default-round-down"), 1, 1, 1, 1);
+
+
+
         loadingBar = new NinePatchDrawable(loadingBarPatch);
         loadingBarBackground = new NinePatchDrawable(loadingBarBackgroundPatch);
     }
 
-
-    public void update(float dt) {
-
-    }
+    public void update(float dt) {}
 
     public void draw(Batch batch) {
-        loadingBarBackground.draw(batch, 00.0040f, 00.0040f, 0.0040f,0.0040f);
+        loadingBarBackground.draw(batch, 0.4f, 0.4f, 0.4f,0.4f);
         loadingBar.draw(batch, 0.004f, 0.004f, Player.bob_health,0.004f);
     }
 
