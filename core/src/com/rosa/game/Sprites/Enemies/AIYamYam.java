5c651d04ad52e2dbab75de90260ab1a7976dd46f
adamdelarosa <gojiradam@gmail.com>
2016-12-13 01:03:22 +0200

    Main Menu

--- core/src/com/rosa/game/Sprites/Enemies/AIYamYam.java
+++ core/src/com/rosa/game/Sprites/Enemies/AIYamYam.java
@@ -236,23 +236,14 @@ public class AIYamYam extends Enemy {
         rayShapeTwo.setRadius(6 / Application.PPM);
         fixtureDefRayTwo.filter.categoryBits = Application.RAY_TWO_INNER;
 
-
-//        fixtureDefRayTwo.shape = rayShapeTwo;
-//        fixtureDefRayTwo.isSensor = true;
-//        rayShapeTwo.setPosition(new Vector2(0.2f, 0 / Application.PPM));
-//        b2body.createFixture(fixtureDefRayTwo).setUserData(this);
-//        rayShapeTwo.setPosition(new Vector2(-0.2f, 0 / Application.PPM));
-//        b2body.createFixture(fixtureDefRayTwo).setUserData(this);
-
-
-                fixtureDefRayTwo.shape = rayShapeTwo;
+        fixtureDefRayTwo.shape = rayShapeTwo;
         fixtureDefRayTwo.isSensor = true;
         rayShapeTwo.setPosition(new Vector2(0.2f, 0 / Application.PPM));
         b2body.createFixture(fixtureDefRayTwo).setUserData(this);
         rayShapeTwo.setPosition(new Vector2(-0.2f, 0 / Application.PPM));
         b2body.createFixture(fixtureDefRayTwo).setUserData(this);
 
-
+        //FixtureDef fixtureDefRayTwo = new FixtureDef()
         sr.begin(ShapeRenderer.ShapeType.Line);
         sr.line(p1, p2);
         sr.line(collision, normal);
