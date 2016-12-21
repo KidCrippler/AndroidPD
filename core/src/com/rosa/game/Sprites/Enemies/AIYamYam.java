f7ae5ab1fa7c0237dec0ad572682e8ca836832ca
adamdelarosa <gojiradam@gmail.com>
2016-12-21 22:16:27 +0200

    Menu Build

--- core/src/com/rosa/game/Sprites/Enemies/AIYamYam.java
+++ core/src/com/rosa/game/Sprites/Enemies/AIYamYam.java
@@ -157,7 +157,6 @@ public class AIYamYam extends Enemy {
         };
 
         world.rayCast(callback, b2body.getPosition(), new Vector2(b2body.getPosition().x - 500, b2body.getPosition().y));
-        edgeShapeRealRayCast.set(new Vector2(b2body.getPosition().x, b2body.getPosition().x), new Vector2(b2body.getPosition().x, b2body.getPosition().x ));
 
     }
 
@@ -286,7 +285,9 @@ public class AIYamYam extends Enemy {
         b2body.createFixture(FixtureRealRayCast).setUserData(this);
 
 //        edgeShapeRealRayCast.set(new Vector2(0 / Application.PPM, 48 / Application.PPM), new Vector2(-100 / Application.PPM, 48 / Application.PPM));
-//        b2body.createFixture(FixtureRealRayCast).setUserData(this);
+        edgeShapeRealRayCast.set(new Vector2(b2body.getPosition().x, b2body.getPosition().x), new Vector2(b2body.getPosition().x, b2body.getPosition().x ));
+
+        b2body.createFixture(FixtureRealRayCast).setUserData(this);
     }
 
     @Override
